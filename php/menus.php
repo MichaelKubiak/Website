52c751a6937ca6102e0e95bc4e753b8625e9ac1c
michaelkubiak <michael.kubiak@bath.edu>
2020-06-25 11:19:11 +0100

    create recursive file system parser

---
+++ php/menus.php
@@ -0,0 +1,50 @@
+<?php
+    function getfiles($path){
+        $out = array();
+        foreach (glob("$path/*") as $filename){
+            $p = pathinfo($filename);
+            $out[] = $p;
+        }
+        return $out;
+    }
+
+    function getMenuLists($path = ".", $i = 0){
+        if ($i >10)
+            return;
+        $pages = array();
+        foreach (getfiles("$path") as $page){
+            if ($page[extension] == "html" || $page[extension] == "php"){
+                $name = getMetaName($path, $page);
+                
+                if ($path)
+                    $name = "$path/$name";
+                array_push($pages, "$name");
+            } else if (!$page[extension]){
+                
+                array_push($pages, getMenuLists("$path/$page[filename]", $i+1));
+
+            }
+
+        }
+        return $pages;
+    }
+
+    function getMetaName($path, $page){
+        $pagecode = file_get_contents("$path/$page[filename].$page[extension]");
+                
+        $domdoc = new DOMDocument;
+
+        @$domdoc -> loadHTML($pagecode);
+        $meta = $domdoc->getElementByID("name");
+        $name = $meta->getAttribute("name");
+        return $name;
+    }
+
+    function getCurrentPage(){
+        
+        echo $_SERVER["PHP_SELF"];
+    }
+?>
+
+
+
