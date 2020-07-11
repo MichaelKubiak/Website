01e674442c97ccb6c01d57a381c834e36da605a6
michaelkubiak <michael.kubiak@bath.edu>
2020-07-11 15:33:48 +0100

    write planning file to better structure work

--- php/menus.php
+++ php/menus.php
@@ -1,7 +1,5 @@
 <?php
 
-    include("./constants.php");
-
     function getfiles($path){
         $out = array();
         foreach (glob("$path/*") as $filename){
@@ -16,38 +14,39 @@
         if ($i >10)
             return;
         $pages = array();
+        $new = false;
         foreach (getfiles($path) as $page){
-            if ($page[extension] == "html" || $page[extension] == "php" || $page[extension] == "phtml"){
+            if (!isset($page['extension'])){
+                
+                array_push($pages, getMenuLists("$path/$page[filename]", $i+1));
 
+            }
+            else if ($page['extension'] == "html" || $page['extension'] == "phtml"){
                 $name = getMetaName($path, $page);
                 
                 if ($path)
                     $name = "$path/$name";
                 array_push($pages, "$name");
-            } else if (!$page[extension]){
-                
-                array_push($pages, getMenuLists("$path/$page[filename]", $i+1));
-
             }
-
         }
         return $pages;
     }
 
     function getMetaName($path, $page){
-        $pagecode = file_get_contents("$path/$page[filename].$page[extension]");
+        $pagecode = file_get_contents("$path/$page[basename]");
         $domdoc = new DOMDocument;
 
         @$domdoc -> loadHTML($pagecode);
         $meta = $domdoc->getElementByID("name");
         $name = $meta->getAttribute("name");
+
         return $name;
     }
 
     function getCurrentPage(){
         
         $page = pathinfo($_SERVER['PHP_SELF']);
-        $path = _ROOT . $page[dirname];
+        $path = _ROOT . $page['dirname'];
         $name = getMetaName($path, $page);
         return "$path/$name";
     }
@@ -58,7 +57,7 @@
                 $style = "this_menu";
             else
                 $style = "other_menu";
-            $splitpage = split("\\", $page);
+            $splitpage = explode("/", $page);
             $name = array_pop($splitpage);
             echo "<a class=$style href=$page>$name</a>";
         }
